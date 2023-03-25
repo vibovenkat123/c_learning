5481f1dee4fd85ad88f056ebd92bc61db931a94a
Vaibhav Venkat <vibovenkat@gmail.com>
2023-03-24 21:43:34 -0500

    feat: add echo implementation in c

---
+++ echo/cecho.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+void echo(char msg[]) { printf("%s", msg); }
+
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    echo(argv[1]);
+    if (strcmp(argv[1], "-n") != 0) {
+      printf("\n");
+    }
+  } else if (argc > 2) {
+    int i = 1;
+    if (strcmp(argv[1], "-n") == 0) {
+      i = 2;
+    }
+    for (; i < argc; ++i) {
+      echo(argv[i]);
+      if (i != argc - 1) {
+        printf(" ");
+      } else if (strcmp(argv[1], "-n") != 0) {
+        printf("\n");
+      }
+    }
+  } else {
+    printf(" ");
+  }
+  return 0;
+}
